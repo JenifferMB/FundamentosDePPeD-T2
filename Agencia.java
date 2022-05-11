243e3c8582ea0e0a47976aa93ce441c8e723c454
JENIFFER MOREIRA BORGES <JENIFFER.BORGES@edu.pucrs.br>
2022-05-11 19:59:00 -0300

    escopo agencia

---
+++ Agencia.java
@@ -0,0 +1,14 @@
+/**
+    Interface Agencia com assinatura dos métodos 
+ */
+
+import java.rmi.*;
+
+public interface Agencia extends Remote {
+    public int abrirConta (String nome, int cpf) throws RemoteException;
+    public Boolean fecharConta (int idConta) throws RemoteException;
+    public Boolean autenticarConta (double x, double y) throws RemoteException;
+    public Double depositar (double deposita) throws RemoteException;
+    public Double retirar (double retira) throws RemoteException;
+    public Double saldo (int x) throws RemoteException;
+}
---
+++ AgenciaImp.java
@@ -0,0 +1,51 @@
+/**
+ * Implementação das funções assinadas na interface
+ */
+
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+
+public class AgenciaImp extends UnicastRemoteObject implements Agencia{
+
+    protected AgenciaImp() throws RemoteException {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int abrirConta(String nome, int cpf) throws RemoteException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Boolean fecharConta(int idConta) throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean autenticarConta(double x, double y) throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double depositar(double deposita) throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double retirar(double retira) throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double saldo(int x) throws RemoteException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
