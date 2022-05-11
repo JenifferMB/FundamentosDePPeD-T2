2f260b9adaf9f7d07c86eaee51263fbbd11bdc87
JENIFFER MOREIRA BORGES <JENIFFER.BORGES@edu.pucrs.br>
2022-05-11 20:00:08 -0300

    escopo agencia

--- Agencia.java
+++ Agencia.java
@@ -7,8 +7,8 @@ import java.rmi.*;
 public interface Agencia extends Remote {
     public int abrirConta (String nome, int cpf) throws RemoteException;
     public Boolean fecharConta (int idConta) throws RemoteException;
-    public Boolean autenticarConta (double x, double y) throws RemoteException;
+    public Boolean autenticarConta (int idConta) throws RemoteException;
     public Double depositar (double deposita) throws RemoteException;
     public Double retirar (double retira) throws RemoteException;
-    public Double saldo (int x) throws RemoteException;
+    public Double saldo (int idConta) throws RemoteException;
 }
