25afce63521a135774f6feacf31059e3af133416
JENIFFER MOREIRA BORGES <JENIFFER.BORGES@edu.pucrs.br>
2022-05-16 20:36:34 -0300

    Server

--- AdministracaoImp.java
+++ AdministracaoImp.java
@@ -4,49 +4,59 @@
 
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 
 public class AdministracaoImp extends UnicastRemoteObject implements Administracao{
 
+    HashMap<String, Conta> map = new HashMap<>();
     protected AdministracaoImp() throws RemoteException {
         super();
         //TODO Auto-generated constructor stub
-    }
 
+    }
     @Override
-    public Boolean abrirConta(String nome, int cpf, char agencia) throws RemoteException {
-        //if()
-        //Conta c = new Conta(nome, id, saldo);
+    public Boolean abrirConta(String nome, String cpf, Boolean agencia) throws RemoteException {
+        if(agencia){
+            if(autenticarConta(cpf)){return false;}
+            Conta c = new Conta(nome, cpf);
+            map.put(cpf, c);
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public Boolean fecharConta(String cpf, Boolean agencia) throws RemoteException {
+        if(agencia){
+            if(!autenticarConta(cpf)){return false;}
+            map.remove(cpf);
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public Boolean autenticarConta(String cpf) throws RemoteException {
+        if(map.containsKey(cpf)){return true;}
+        return false;
+    }
+    
+    @Override
+    public Boolean depositar(double deposita, String cpf) throws RemoteException {
+        // TODO Auto-generated method stub
         return null;
     }
-
+    
     @Override
-    public Boolean fecharConta(int idConta, char agencia) throws RemoteException {
+    public Boolean retirar(double retira, String cpf) throws RemoteException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
-    public Boolean autenticarConta(int idConta) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
-    @Override
-    public Boolean depositar(double deposita) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Boolean retirar(double retira) throws RemoteException {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return "AdministracaoImp []";
-    }
+  
 
     
 }
